01ad1865b989047c3455aaf51f89e962eda14b47
Mustafa Paksoy <paksoy@gmail.com>
2014-10-12 17:57:19 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to little-clojurer
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/little_clojurer/core_test.clj
@@ -0,0 +1,7 @@
+(ns little-clojurer.core-test
+  (:require [clojure.test :refer :all]
+            [little-clojurer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
