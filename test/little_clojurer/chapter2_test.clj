3ceaf8c88539d0b47621a3e25036728f859280b1
Mustafa Paksoy <paksoy@gmail.com>
2014-10-16 22:34:16 -0700

    Add tests for member?

--- test/little_clojurer/chapter2_test.clj
+++ test/little_clojurer/chapter2_test.clj
@@ -9,3 +9,13 @@
     (is (lat? '()))
     (is (lat? '(:a)))
     (is (lat? '(:a 1)))))
+
+(deftest member?-test
+  (testing
+    (is (not (member? :a '())))
+    (is (not (member? 1 '())))
+    (is (not (member? '() '('()))))
+    (is (member? :a '(:a :a :a)))
+    (is (member? 1 '(:a 1 :a)))
+    (is (member? :a '(1 1 1 :a)))))
+
