7fd5872b2852153a4ba41369fb5ae75190eb3bf5
Mustafa Paksoy <mustafa@optimizely.com>
2014-11-16 13:42:05 -0800

    Add test for pick.

--- test/little_clojurer/chapter4_test.clj
+++ test/little_clojurer/chapter4_test.clj
@@ -20,3 +20,10 @@
     (is (= '(1) (tup+ '(1) '(0))))
     (is (= '(1 0) (tup+ '(1 1) '(0 -1))))
     (is (= '(1 2 3 4) (tup+ '(1 2 3 4) '(0 0 0 0))))))
+
+
+(deftest pick-test
+  (testing
+    (is (= 1 (pick 1 (range 10))))
+    (is (= 5 (pick 5 (range 10))))
+    (is (= :b (pick 1 '(:a :b :c))))))
