a016b4f3041fe4069740e8d18661d304bbaa7682
Mustafa Paksoy <mustafa@optimizely.com>
2014-11-16 13:25:55 -0800

    Add chapter 4 stubs.

---
+++ src/little_clojurer/chapter4.clj
@@ -0,0 +1,43 @@
+(ns little-clojurer.chapter4
+  (:require [little-clojurer.chapter3 :refer :all]))
+
+(defn addtup
+  "Sum all numbers in a list, return total."
+  [tup]
+  nil)
+
+
+(defn tup+
+  "Takes two tuples of the same length, return a new tuple with the same length where each item is the sum of two items from the original tuples with the same index."
+  [tup1 tup2]
+  nil)
+
+
+(defn pick
+  "Return item in the given index of given table."
+  [index l]
+  nil)
+
+
+(defn rempick
+  "Return list with item in the given index removed."
+  [index l]
+  nil)
+
+
+(defn no-nums
+  "Return l with all numbers removed."
+  [l]
+  nil)
+
+
+(defn all-nums
+  "Return l with all non-numbers removed."
+  [l]
+  nil)
+
+
+(defn occur
+  "Return the number of times given item occurs in given list."
+  [item l]
+  nil)
