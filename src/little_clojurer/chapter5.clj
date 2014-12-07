ca25a5beba59860736c520c5ec2a616d357d5266
Mustafa Paksoy <paksoy@gmail.com>
2014-12-07 08:23:19 -0800

    Add docstrings to chapter 5 stubs.

--- src/little_clojurer/chapter5.clj
+++ src/little_clojurer/chapter5.clj
@@ -3,35 +3,36 @@
 
 
 (defn rember*
+  "find all occurrances of a in l deeply recursively and remove them"
   [a l]
   nil)
 
 
 (defn insertR*
+  "find all occurrances of old in l deeply recursively and append new to the right of the occurrances"
   [new old l]
   nil)
 
 
 (defn occur*
+  "count the number of times a occurs in l deeply recursively"
   [a l]
   nil)
 
 
 (defn subst*
-  [new old l]
-  nil)
-
-
-(defn insertL*
+  "find all occurrances of a in l deeply recursively and replace them with new"
   [new old l]
   nil)
 
 
 (defn member*
+  "check if a occurs in l deeply recursively"
   [a l]
   nil)
 
 
 (defn leftmost
+  "finds the leftmost atom in l deeply recursively. l cannot be empty and cannot contain empty lists."
   [l]
   nil)
