fddd49421b227d639f555c40a76733bfd6853d73
Mustafa Paksoy <paksoy@gmail.com>
2014-12-07 07:40:25 -0800

    Chapter 5 stubs.

---
+++ src/little_clojurer/chapter5.clj
@@ -0,0 +1,37 @@
+(ns little-clojurer.chapter5
+  (:require [little-clojurer.chapter4 :refer :all]))
+
+
+(defn rember*
+  [a l]
+  nil)
+
+
+(defn insertR*
+  [new old l]
+  nil)
+
+
+(defn occur*
+  [a l]
+  nil)
+
+
+(defn subst*
+  [new old l]
+  nil)
+
+
+(defn insertL*
+  [new old l]
+  nil)
+
+
+(defn member*
+  [a l]
+  nil)
+
+
+(defn leftmost
+  [l]
+  nil)
